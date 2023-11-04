c59a132a379ccdedf360cef9d00669ceaac0ff25
Dominic Furchert <dominic.furchert@edu.tbz.ch>
2023-11-04 16:42:16 +0100

    set up prod env

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,8 @@
 INSERT INTO role(name) VALUES('ROLE_USER');
 INSERT INTO role(name) VALUES('ROLE_MODERATOR');
 INSERT INTO role(name) VALUES('ROLE_ADMIN');
-INSERT INTO userStatus(name) values ('ACTIVE');
-INSERT INTO userStatus(name) values ('INACTIVE');
-INSERT INTO userStatus(name) values ('BLOCKED');
+INSERT INTO status(name) values ('UNVERIFIED');
+INSERT INTO status(name) values ('ACTIVE');
+INSERT INTO status(name) values ('INACTIVE');
+INSERT INTO status(name) values ('BLOCKED');
 */
